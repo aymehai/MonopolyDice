d6e331d875ce641b10b6f9c1e01a1d17186ad743
MehSwitch <aymehaiDev@protonmail.com>
2018-06-19 14:31:25 -0400

    Finished Monopoly Project

--- MonopolyDice/ViewController.swift
+++ MonopolyDice/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftDice: UIImageView!
+    @IBOutlet weak var rightDice: UIImageView!
+    @IBOutlet weak var totalDice: UILabel!
+    @IBOutlet weak var diceButton: UIButton!
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        let randomNum1 = Int(arc4random_uniform(5))+1
+        let randomNum2 = Int(arc4random_uniform(5))+1
+        
+        if(randomNum1 == randomNum2){
+            totalDice.text = String(randomNum1 + randomNum2)
+            totalDice.textColor = UIColor.red
+        }
+        else{
+            totalDice.text = String(randomNum1 + randomNum2)
+            totalDice.textColor = UIColor.black
+            
+        }
+        
+        
+        leftDice.image = UIImage(named: "dice\(randomNum1).jpg")
+        rightDice.image = UIImage(named: "dice\(randomNum2).jpg")
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
